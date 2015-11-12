665fbc39a78802f3d9a5363f7f6a5bfc354fbbf2
Łukasz Lalik <lukasz.lalik@gmail.com>
2015-11-12 22:20:49 +0100

    Distinguish function which builds request headers

--- src/algolia_transport.erl
+++ src/algolia_transport.erl
@@ -4,14 +4,18 @@
 
 build_request(Method, Host, Path, AppId, ApiKey) ->
   Url = lists:flatten(io_lib:format("https://~s~s", [Host, Path])),
+  Headers = build_headers(AppId, ApiKey),
   [
     {method, Method},
     {url, Url},
-    {headers, [
-      {"Content-Type", "application/json; charset=utf-8"},
-      {"X-Algolia-Application-Id", AppId},
-      {"X-Algolia-API-Key", ApiKey},
-      {"Connection", "keep-alive"},
-      {"User-Agent", "Algolia for Erlang"}
-    ]}
+    {headers, Headers}
+  ].
+
+build_headers(AppId, ApiKey) ->
+  [
+    {"Content-Type", "application/json; charset=utf-8"},
+    {"X-Algolia-Application-Id", AppId},
+    {"X-Algolia-API-Key", ApiKey},
+    {"Connection", "keep-alive"},
+    {"User-Agent", "Algolia for Erlang"}
   ].
