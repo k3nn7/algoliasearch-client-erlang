cade880a9f6ad33925224e016fc14efab542a82b
Łukasz Lalik <lukasz.lalik@gmail.com>
2016-01-23 13:06:47 +0100

    Refactor algolia_index:add_object/2

--- test/algolia_index_test.erl
+++ test/algolia_index_test.erl
@@ -3,49 +3,23 @@
 -include_lib("eunit/include/eunit.hrl").
 
 add_object_test() ->
-  Client = algolia:make_client("foo", "bar"),
+  Object = #{<<"content">> => <<"foo bar">>},
+  ExpectedRequest = {write, post, "/1/indexes/baz", Object},
+  ExpectedResult = {ok, #{<<"objectID">> => <<"213">>}},
+  Client = algolia_mock_client:make(ExpectedRequest, ExpectedResult),
   Index = algolia:init_index(Client, "baz"),
-  Object = {[
-    {<<"content">>, <<"foo bar">>}
-  ]},
-  ?assertEqual(
-    [
-      {method, post},
-      {url, "https://foo.algolia.net/1/indexes/baz"},
-      {body, <<"{\"content\":\"foo bar\"}">>},
-      {headers, [
-        {"Content-Type", "application/json; charset=utf-8"},
-        {"X-Algolia-Application-Id", "foo"},
-        {"X-Algolia-API-Key", "bar"},
-        {"Connection", "keep-alive"},
-        {"User-Agent", "Algolia for Erlang"}
-      ]}
-    ],
-    algolia_index:add_object_request(Index, Object)
-  ).
+  ?assertEqual(ExpectedResult, algolia_index:add_object(Index, Object)).
 
 add_object_with_given_id_test() ->
-  Client = algolia:make_client("foo", "bar"),
+  Object = #{
+    <<"objectID">> => <<"4321">>,
+    <<"content">> => <<"foo bar">>
+  },
+  ExpectedRequest = {write, put, "/1/indexes/baz/4321", Object},
+  ExpectedResult = {ok, #{<<"objectID">> => <<"213">>}},
+  Client = algolia_mock_client:make(ExpectedRequest, ExpectedResult),
   Index = algolia:init_index(Client, "baz"),
-  Object = {[
-    {<<"objectID">>, <<"4321">>},
-    {<<"content">>, <<"foo bar">>}
-  ]},
-  ?assertEqual(
-    [
-      {method, put},
-      {url, "https://foo.algolia.net/1/indexes/baz/4321"},
-      {body, <<"{\"objectID\":\"4321\",\"content\":\"foo bar\"}">>},
-      {headers, [
-        {"Content-Type", "application/json; charset=utf-8"},
-        {"X-Algolia-Application-Id", "foo"},
-        {"X-Algolia-API-Key", "bar"},
-        {"Connection", "keep-alive"},
-        {"User-Agent", "Algolia for Erlang"}
-      ]}
-    ],
-    algolia_index:add_object_request(Index, Object)
-  ).
+  ?assertEqual(ExpectedResult, algolia_index:add_object(Index, Object)).
 
 update_object_test() ->
   Client = algolia:make_client("foo", "bar"),
